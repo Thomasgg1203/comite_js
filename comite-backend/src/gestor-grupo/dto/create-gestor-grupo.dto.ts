f4a5c3e375ee00cc43a3ab12072dcd1eef6e980c
Sergio Alejandro <110745388+SergioA515@users.noreply.github.com>
2023-10-23 04:54:05 -0500

    feat<global>:validacion dto's, creación esquemas y mongoose

--- comite-backend/src/gestor-grupo/dto/create-gestor-grupo.dto.ts
+++ comite-backend/src/gestor-grupo/dto/create-gestor-grupo.dto.ts
@@ -1,7 +1,20 @@
+import {IsNotEmpty,Length, IsEmail} from 'class-validator';
 export class CreateGestorGrupoDto {
+    
+    @IsNotEmpty()
     nombres:string;
+    
+    @IsNotEmpty()
     apellidos:string;
+    
+    @IsNotEmpty()
+    @Length(5,15)
     documento:string;
+    
+    @IsEmail()
     correo:string;
+
+    @IsNotEmpty()
+    @Length(10,15)
     telefono:string;
 }
--- comite-backend/src/programas/dto/create-programa.dto.ts
+++ comite-backend/src/programas/dto/create-programa.dto.ts
@@ -1,5 +1,12 @@
+import {IsNotEmpty} from 'class-validator';
 export class CreateProgramaDto {
+    
+    @IsNotEmpty()
     codigo:string;
+    
+    @IsNotEmpty()
     nombre:string;
+    
+    @IsNotEmpty()
     nivel_formacion:string;
 }
