7c003dc5f261f27c50f6634230b50aa2b9c0f051
Thomasgg1203 <thomasgg1203@gmail.com>
2023-11-08 22:23:44 -0500

    feat(reglamento) relacion entre capitulo y articulo

--- comite-frontend/src/api/articulo/index.js
+++ comite-frontend/src/api/articulo/index.js
@@ -5,3 +5,14 @@ import axios from "axios";
 const baseURL = `http://localhost:4000`;
 
 export const getAllArticulos = async () => await axios.get(`${baseURL}/articulos`);
+
+//Parar traer articulo, por articulo
+export const GetArticulosByCapituloId = async (capituloId) => {
+  try {
+    const response = await axios.get(`${baseURL}/articulos?capituloId=${capituloId}`);
+    return response.data;
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    return [];
+  }
+};
--- comite-frontend/src/layouts/reglamento/index.js
+++ comite-frontend/src/layouts/reglamento/index.js
@@ -11,23 +11,23 @@ import Grid from "@mui/material/Grid";
 //Parte de tipografia
 import MDTypography from "components/MDTypography";
 //contexto
-import { capitulosData } from "context";
+import { reglamento } from "context";
 
 const Reglamento = () => {
-  const [capitulos, setCapitulos] = useState([]);
+  const [datos, setDatos] = useState([]);
 
   useEffect(() => {
     const dataAll = async () => {
       //Porque es lo que se esta respondiendo el backend
-      const responts = await capitulosData();
-      console.log(responts.data);
-      setCapitulos(responts.data);
+      const responts = await reglamento();
+      console.log(responts);
+      setDatos(responts);
     };
     dataAll();
-  }, [capitulosData]);
+  }, [reglamento]);
 
   //si capitulos no ejecuta
-  if (!capitulos || capitulos.length === 0) {
+  if (!datos || datos.length === 0) {
     return (
       <DashboardLayout>
         <DashboardNavbar />
@@ -66,51 +66,36 @@ const Reglamento = () => {
       <MDBox pt={6} pb={3}>
         <Grid container spacing={6}>
           <Grid item xs={12}>
-            <MDBox mx={2} mt={-3} py={3} px={2} bgColor="white" borderRadius="lg">
-              <MDTypography variant="h3">Reglamento Del Aprendiz</MDTypography>
-              <br />
-              <MDTypography variant="h5" color="black">
-                {`${capitulos[0].capitulo}, ${capitulos[0].titulo}`}
-              </MDTypography>
-              <br />
-              <MDTypography variant="h6" color="black">
-                Articulo
-              </MDTypography>
-              <MDTypography variant="body2" color="black">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere nunc ut augue
-                aliquet, at consequat tellus placerat. Pellentesque habitant morbi tristique
-                senectus et netus et malesuada fames ac turpis egestas. Aliquam egestas velit a diam
-                accumsan ullamcorper. Fusce condimentum purus quis turpis laoreet, id ullamcorper
-                arcu mattis. Pellentesque rhoncus, ex a venenatis gravida, lorem diam laoreet justo,
-                ut ultrices urna justo eget quam. Quisque in consequat dui, volutpat ornare erat.
-                Phasellus eu pulvinar mi, et sagittis velit. Maecenas scelerisque tortor ut blandit
-                porta. Nulla commodo, leo ac aliquam dignissim, turpis odio condimentum lorem, at
-                elementum velit metus ac erat. In quis lorem aliquet turpis varius placerat. In
-                euismod lorem turpis
-              </MDTypography>
-              <br />
-              <MDTypography variant="body2" color="black">
-                1. Numeral
-              </MDTypography>
-              <MDTypography variant="body2">1. Numeral </MDTypography>
-              <MDTypography variant="body2">1. Numeral </MDTypography>
-              <MDTypography variant="body2">1. Numeral </MDTypography>
-              <MDTypography variant="body2">1. Numeral </MDTypography>
-              <br />
-              <MDTypography variant="h6">Parágrafo</MDTypography>
-              <MDTypography variant="body2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin posuere nunc ut augue
-                aliquet, at consequat tellus placerat. Pellentesque habitant morbi tristique
-                senectus et netus et malesuada fames ac turpis egestas. Aliquam egestas velit a diam
-                accumsan ullamcorper. Fusce condimentum purus quis turpis laoreet, id ullamcorper
-                arcu mattis. Pellentesque rhoncus, ex a venenatis gravida, lorem diam laoreet justo,
-                ut ultrices urna justo eget quam. Quisque in consequat dui, volutpat ornare erat.
-                Phasellus eu pulvinar mi, et sagittis velit. Maecenas scelerisque tortor ut blandit
-                porta. Nulla commodo, leo ac aliquam dignissim, turpis odio condimentum lorem, at
-                elementum velit metus ac erat. In quis lorem aliquet turpis varius placerat. In
-                euismod lorem turpis
-              </MDTypography>
-            </MDBox>
+            {datos.map((item) => (
+              <MDBox
+                key={item.capitulo._id}
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                bgColor="white"
+                borderRadius="lg"
+              >
+                <MDTypography variant="h3">Reglamento Del Aprendiz</MDTypography>
+                <br />
+                <MDTypography variant="h5" color="black">
+                  {item.capitulo.capitulo} - {item.capitulo.titulo}
+                </MDTypography>
+                <br />
+                {item.articulos.map((articulo) => (
+                  <div key={articulo._id}>
+                    <MDTypography variant="h6" color="black">
+                      {articulo.titulo}
+                    </MDTypography>
+                    <MDTypography variant="body2" color="black">
+                      {articulo.descripcion}
+                    </MDTypography>
+                    <br />
+                    {/* Aquí puedes agregar los numerales y parágrafos si están disponibles en los datos del artículo */}
+                  </div>
+                ))}
+              </MDBox>
+            ))}
           </Grid>
         </Grid>
       </MDBox>
