e18cf65029ad5113ec7d79948bb1c04f59c76be1
Sergio Alejandro <110745388+SergioA515@users.noreply.github.com>
2023-11-16 01:48:32 -0500

    fix<global>:endpoints &comentarios usuario/ficha/programa

--- comite-backend/src/programas/programas.controller.ts
+++ comite-backend/src/programas/programas.controller.ts
@@ -1,34 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards } from '@nestjs/common';
 import { ProgramasService } from './programas.service';
 import { CreateProgramaDto } from './dto/create-programa.dto';
 import { UpdateProgramaDto } from './dto/update-programa.dto';
-
+import { RoleGuardGuard } from 'src/guards/role-guard.guard';
+import { JwtGuardGuard } from 'src/guards/jwt-guard.guard';
+// uso del guard con solo llamar su decorador, brindamos la metadata de los guardas que tenemos definidos
+@UseGuards(JwtGuardGuard, RoleGuardGuard)
 @Controller('programas')
 export class ProgramasController {
   constructor(private readonly programasService: ProgramasService) {}
-
-  @Post()
+  // ${baseURL}/programas/crear
+  @Post('crear')
   @HttpCode(201)
   create(@Body() createPrograma: CreateProgramaDto) {
     return this.programasService.create(createPrograma);
   }
-
-  @Get()
+  // ${baseURL}/programas/obtener
+  @Get('obtener')
   findAll() {
     return this.programasService.findAll();
   }
-
-  @Get(':id')
+  // ${baseURL}/programas/obtener:id?(_TODO_:aquí literal es el id, pero cambiara para obtener por medio de documento o usuario)
+  @Get('obtener:id')
   findOne(@Param('id') id: string) {
     return this.programasService.findOne(+id);
   }
-
-  @Patch(':id')
+  // ${baseURL}/programas/actualizar:id?(_TODO_:aquí literal es el id, pero cambiara para obtener por medio de documento o usuario)
+  @Patch('actualizar:id')
   update(@Param('id') id: string, @Body() updateProgramaDto: UpdateProgramaDto) {
     return this.programasService.update(+id, updateProgramaDto);
   }
-
-  @Delete(':id')
+  // ${baseURL}/programas/eliminar:id?(_TODO_:aquí literal es el id, pero cambiara para obtener por medio de documento o usuario)
+  @Delete('eliminar:id')
   remove(@Param('id') id: string) {
     return this.programasService.remove(id);
   }
