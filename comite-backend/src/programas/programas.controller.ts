99f94bf510ce69a38d9306d86ad59eadc763c347
Sergio Alejandro <110745388+SergioA515@users.noreply.github.com>
2023-11-28 19:56:28 -0500

    fix<global>:arreglo de metodos de consulta y creación programa y fichas

--- comite-backend/src/programas/programas.controller.ts
+++ comite-backend/src/programas/programas.controller.ts
@@ -7,33 +7,38 @@ import { JwtGuardGuard } from 'src/guards/jwt-guard.guard';
 import { Rol } from 'src/decorators/rol.decorator';
 // uso del guard con solo llamar su decorador, brindamos la metadata de los guardas que tenemos definidos
 @UseGuards(JwtGuardGuard, RoleGuardGuard)
-@Rol(['administrador','aprendiz','gestor-grupo','gestor-comite'])
 @Controller('programas')
 export class ProgramasController {
   constructor(private readonly programasService: ProgramasService) {}
   // ${baseURL}/programas/crear
   @Post('crear')
+  @Rol(['administrador','gestor-grupo','gestor-comite'])
   @HttpCode(201)
   create(@Body() createPrograma: CreateProgramaDto) {
     return this.programasService.create(createPrograma);
   }
   // ${baseURL}/programas/obtener
   @Get('obtener')
+  @Rol(['administrador','aprendiz','gestor-grupo','gestor-comite'])
   findAll() {
     return this.programasService.findAll();
   }
   // ${baseURL}/programas/obtener:id?(_TODO_:aquí literal es el id, pero cambiara para obtener por medio de documento o usuario)
-  @Get('obtener:id')
+  @Get('obtener/:id')
+  @Rol(['administrador','aprendiz','gestor-grupo','gestor-comite'])
   findOne(@Param('id') id: string) {
-    return this.programasService.findOne(+id);
+    const programa = this.programasService.findOne(id);
+    return programa;
   }
   // ${baseURL}/programas/actualizar:id?(_TODO_:aquí literal es el id, pero cambiara para obtener por medio de documento o usuario)
-  @Patch('actualizar:id')
+  @Patch('actualizar:/id')
+  @Rol(['administrador','gestor-grupo','gestor-comite'])
   update(@Param('id') id: string, @Body() updateProgramaDto: UpdateProgramaDto) {
-    return this.programasService.update(+id, updateProgramaDto);
+    return this.programasService.update(id, updateProgramaDto);
   }
   // ${baseURL}/programas/eliminar:id?(_TODO_:aquí literal es el id, pero cambiara para obtener por medio de documento o usuario)
-  @Delete('eliminar:id')
+  @Delete('eliminar:/id')
+  @Rol(['administrador','gestor-grupo','gestor-comite'])
   remove(@Param('id') id: string) {
     return this.programasService.remove(id);
   }
