d8c27c72e4b5604dd156b062871467eea3876fa0
Sergio Alejandro <110745388+SergioA515@users.noreply.github.com>
2023-11-30 03:48:58 -0500

    Revert "Dev ospina 1"

--- comite-backend/src/solicitudes/solicitudes.controller.ts
+++ comite-backend/src/solicitudes/solicitudes.controller.ts
@@ -1,12 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile,UseGuards, Res, Header } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
 import { SolicitudesService } from './solicitudes.service';
 import { CreateSolicitudeDto } from './dto/create-solicitude.dto';
 import { UpdateSolicitudeDto } from './dto/update-solicitude.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { storage } from 'src/utils/media.handle';
-import * as fs from 'fs';
-import path from 'path';
-import e from 'express';
 import { JwtGuardGuard } from 'src/guards/jwt-guard.guard';
 import { RoleGuardGuard } from 'src/guards/role-guard.guard';
 import { Rol } from 'src/decorators/rol.decorator';
@@ -34,31 +31,12 @@ export class SolicitudesController {
 
   @Get('obtener:id')
   findOne(@Param('id') id: string) {
-    return this.solicitudesService.findOne(id);
+    return this.solicitudesService.findOne(+id);
   }
 
-  @Get('/archivo/:nombre')
-  @Header('Content-Type', 'application/octet-stream')
-  @Header('Content-Disposition', `attachment; filename=${"archivo"}`)
-  async obtenerArchivo(@Param('nombre') nombre: string, @Res() res: Response) {
-  const ruta = __dirname + "../../tmp/" + nombre;
-  console.log(ruta);
-  fs.access(ruta, fs.constants.F_OK, (err) => {
-    if (err) {
-      console.log("Err", err);
-    } else {
-
-      const archivo = fs.createReadStream(ruta);
-      archivo.pipe(res as any);
-    }
-  });
-  return ruta;
-}
-
-
-  @Patch(':id')
+  @Patch('actualizar:id')
   update(@Param('id') id: string, @Body() updateSolicitudeDto: UpdateSolicitudeDto) {
-    return this.solicitudesService.update(id, updateSolicitudeDto);
+    return this.solicitudesService.update(+id, updateSolicitudeDto);
   }
 
   @Delete('eliminar:id')
