8659ac789292aaefc448c4e0775261d3c2018e64
Sergio Alejandro <110745388+SergioA515@users.noreply.github.com>
2023-12-05 04:11:39 -0500

    feat<module>:aceptar solicitud

--- comite-backend/src/solicitudes/dto/update-solicitude.dto.ts
+++ comite-backend/src/solicitudes/dto/update-solicitude.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSolicitudeDto } from './create-solicitude.dto';
+import { IsBoolean } from 'class-validator';
 
-export class UpdateSolicitudeDto extends PartialType(CreateSolicitudeDto) {}
+export class UpdateSolicitudeDto extends PartialType(CreateSolicitudeDto) {
+    
+    @IsBoolean()
+    estado_solicitud: boolean;
+
+}
